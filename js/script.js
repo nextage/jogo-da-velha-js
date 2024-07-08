7102f6d5ce73ab4c595db5ad46ddb9cdc00870f4
nextage <exegabriel086@gmail.com>
2024-07-08 15:26:48 -0300

    Criação da função de clique na célula do tabuleiro

---
+++ js/script.js
@@ -0,0 +1,52 @@
+//seleciona todos os elementos com a classe "cell" - (celulas do tabuleiro)
+const cells = document.querySelectorAll('.cell');
+
+//Seleciona o botão de reiniciar o jogo
+const restarButton = document.querySelector('.restart-btn');
+
+//seleciona onde o status do jogo será exibido
+const gameStatus = document.querySelector('.game-status');
+
+
+//Armazena o estado atual do jogo Array[]
+let gameState = ["","","","","","","","",""];
+
+//define o jogador inicial("X" ou "O")
+let initialPlayer = 'X';
+
+//combinaçoes vencedoras do jogo
+const WinningCombinations = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+//função de clique na celula 
+function cellClick(event) {
+    const cell = event.target;
+    const cellIndex = cell.getAttribute('data-index');
+
+ //verifica se a celula esta prencihada ou se o jogo acabou
+ if (gameState[cellIndex] !== "" || checkWinner()){
+    return;
+ }
+
+ //prenche a celula com o simbolo do jogador atual
+ gameState[cellIndex] = initialPlayer;
+ cell.textContent = initialPlayer;
+
+ //verifica se há um vencedor ou empate
+ if (checkWinner()) {
+    gameStatus.textContent = `jogador ${initialPlayer} venceu!`;
+ } else if (!gameState.includes("")) {
+    gameStatus.textContent = "Empate!";
+ } else{
+    initialPlayer = initialPlayer === 'X' ? 'O' : 'X'
+ }
+
+}
